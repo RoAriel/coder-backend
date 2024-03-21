954354dd48e695108918bd7afea5c5d98a8aee3d
Rodrigo Gonzalez <roariel06@gmail.com>
2024-03-21 19:03:27 -0300

    Entrega 3
    Add ruta y manejo de param y qry param limit

---
+++ entrega_3/src/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+
+const ProductManager = require("../classes/productManager")
+
+const PORT = 3000;
+
+const pm = new ProductManager("../data/products.json")
+
+const app = express();
+
+app.get('/products', async (req, res) =>{
+    let prods = await pm.getProducts();
+    
+    let limit = Number(req.query.limit);  console.log(limit)
+
+    let isNAN = isNaN(limit)
+console.log(isNAN)
+    if(!isNAN && limit>0){
+        prods = prods.slice(0,limit);
+    }
+        
+    res.json(prods);
+
+});
+
+app.get('/products/:pid', async (req,res) =>{
+
+    let pid = Number(req.params.pid)
+    let prod = await pm.getProductById(pid);
+
+    res.json(prod)
+
+
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server online en puerto ${PORT}`)
+})
