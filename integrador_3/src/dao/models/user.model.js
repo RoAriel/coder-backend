eba80d9461027e7fb71a466fee9196bcd5f85d95
Rodrigo Gonzalez <roariel06@gmail.com>
2024-07-13 11:06:07 -0300

    Add bases para integrador_3

---
+++ integrador_3/src/dao/models/user.model.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+
+export const usersModel = mongoose.model('users', new mongoose.Schema({
+    first_name: String,
+    last_name: String,
+    age: Number,
+    email: {
+        type: String, unique: true
+    },
+    password: String,
+    rol: {
+        type: String, default: "user"
+    },
+    cart: {
+        type: mongoose.Types.ObjectId, ref: "carts"
+    }
+},
+    {
+        timestamps: true,
+        strict: false
+    }
+))
